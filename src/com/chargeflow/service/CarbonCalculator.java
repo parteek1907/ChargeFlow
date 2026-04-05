d190b2491b91ed6afb5843fc6e10e51d4e69b9f0
ChargeFlow Dev <dev@chargeflow.com>
2026-04-05 21:51:00 +0530

    style: remove comments from carbon calculator service

--- src/com/chargeflow/service/CarbonCalculator.java
+++ src/com/chargeflow/service/CarbonCalculator.java
@@ -2,19 +2,19 @@ package com.chargeflow.service;
 
 public class CarbonCalculator {
 
-    private static final double EV_EMISSIONS_GRAMS_PER_KM     = 50.0;   
-    private static final double PETROL_EMISSIONS_GRAMS_PER_KM = 120.0;  
-    private static final double DIESEL_EMISSIONS_GRAMS_PER_KM = 110.0;  
+    private static final double EV_EMISSIONS_GRAMS_PER_KM = 50.0;
+    private static final double PETROL_EMISSIONS_GRAMS_PER_KM = 120.0;
+    private static final double DIESEL_EMISSIONS_GRAMS_PER_KM = 110.0;
 
     public double calculateEVEmissions(double distanceKm) {
         double gramsTotal = distanceKm * EV_EMISSIONS_GRAMS_PER_KM;
-        return gramsTotal / 1000.0;  
+        return gramsTotal / 1000.0;
     }
 
     public double calculateICEEmissions(double distanceKm, String fuelType) {
         double gramsPerKm = getEmissionFactor(fuelType);
         double gramsTotal = distanceKm * gramsPerKm;
-        return gramsTotal / 1000.0;  
+        return gramsTotal / 1000.0;
     }
 
     public double calculateSavings(double iceEmissionsKg, double evEmissionsKg) {
@@ -25,6 +25,6 @@ public class CarbonCalculator {
         if ("Diesel".equalsIgnoreCase(fuelType)) {
             return DIESEL_EMISSIONS_GRAMS_PER_KM;
         }
-        return PETROL_EMISSIONS_GRAMS_PER_KM;  
+        return PETROL_EMISSIONS_GRAMS_PER_KM;
     }
 }
