66cd5869fa9d24fe40c8fd5a92b2c09783e6c4ba
ChargeFlow Dev <dev@chargeflow.com>
2026-04-05 21:51:33 +0530

    style: remove comments from motivation score calculator

--- src/com/chargeflow/service/MotivationScoreCalculator.java
+++ src/com/chargeflow/service/MotivationScoreCalculator.java
@@ -2,11 +2,11 @@ package com.chargeflow.service;
 
 public class MotivationScoreCalculator {
 
-    private static final double COST_SAVINGS_WEIGHT   = 0.60;
-    private static final double CO2_REDUCTION_WEIGHT  = 0.40;
+    private static final double COST_SAVINGS_WEIGHT = 0.60;
+    private static final double CO2_REDUCTION_WEIGHT = 0.40;
 
     public int calculateScore(double evCost, double iceCost,
-                              double evEmissionsKg, double iceEmissionsKg) {
+                               double evEmissionsKg, double iceEmissionsKg) {
 
         double costSavingsPercent = 0;
         if (iceCost > 0) {
